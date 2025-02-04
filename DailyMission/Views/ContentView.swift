ea8afac142400f592cf548f7da934ddb121d883a
eminseo2003 <eminseo2003@naver.com>
2025-02-04 11:24:09 +0900

    시작하기 버튼 삭제

--- DailyMission/Views/ContentView.swift
+++ DailyMission/Views/ContentView.swift
@@ -12,7 +12,6 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var missions: [Mission]
     
-    @State private var isNavigated: Bool = false
     
     @State private var newMissionTitle: String = ""
     @State private var showAddMissionAlert: Bool = false
@@ -25,17 +24,7 @@ struct ContentView: View {
                     .bold()
                     .padding(.bottom, 20)
                 
-                Button(action: {
-                    isNavigated = true // ✅ 버튼 클릭 시 ContentView로 이동
-                }) {
-                    Text("시작하기")
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                .padding()
+                
                 NavigationLink(destination: GroupView()) {
                     Text("이동")
                         .frame(maxWidth: .infinity)
