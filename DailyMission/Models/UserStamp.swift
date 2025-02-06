ecef5d1e47799718b63e0a495a8c58f030257b30
hajinCHOI <cresent0203@gmail.com>
2025-02-06 11:22:16 +0900

    userStamp 모델 추가

---
+++ DailyMission/Models/UserStamp.swift
@@ -0,0 +1,20 @@
+//
+//  UserStamp.swift
+//  DailyMission
+//
+//  Created by 최하진 on 2/6/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class UserStamp { // ✅ 유저별 DateStamp를 저장하는 개별 모델
+    var userId : String
+    var dateStamp : [DateStamp] = []
+    init(userId : String, dateStamp : [DateStamp]) {
+        self.userId = userId
+        self.dateStamp = dateStamp
+    }
+}
+
